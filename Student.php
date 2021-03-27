e517f148ec589ee0753653df29b4e9ed8328f494
Youssef <yo.ouadid@gmail.com>
2021-03-27 17:24:31 +0100

    code start up

---
+++ Student.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**
+ * Class Student
+ */
+class Student
+{
+    public string $name;
+    public string $studentNumber;
+
+    // TODO Generate getters and setters for both arguments
+    // TODO Generate constructor with both arguments.
+}
